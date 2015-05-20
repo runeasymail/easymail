b1f8c3315cebc63be9e3687ef31846c3a9abccf8
Gyuner Zeki <me@gzeki.com>
2015-05-21 01:02:16 +0300

    Refactoring autoconfig

--- autoconfig/install.sh
+++ autoconfig/install.sh
@@ -1,11 +1,14 @@
 # Install autoconfig and autodiscover
+
+AUTOCONFIG_DIR="$CURRENT_DIR/autoconfig"
+
 mkdir /usr/share/nginx/autoconfig_and_autodiscover
 
-cp $CURRENT_DIR/autoconfig.php /usr/share/nginx/autoconfig_and_autodiscover/
+cp $AUTOCONFIG_DIR/autoconfig.php /usr/share/nginx/autoconfig_and_autodiscover/
 set_hostname /usr/share/nginx/autoconfig_and_autodiscover/autoconfig.php
 
-cp $CURRENT_DIR/autodiscover.php /usr/share/nginx/autoconfig_and_autodiscover/
+cp $AUTOCONFIG_DIR/autodiscover.php /usr/share/nginx/autoconfig_and_autodiscover/
 set_hostname /usr/share/nginx/autoconfig_and_autodiscover/autodiscover.php
 
-cp $CURRENT_DIR/nginx_config_for_autoconfig_and_autodiscover /etc/nginx/sites-enabled/autoconfig_and_autodiscover
+cp $AUTOCONFIG_DIR/nginx_config /etc/nginx/sites-enabled/autoconfig_and_autodiscover
 set_hostname /etc/nginx/sites-enabled/autoconfig_and_autodiscover
