3ce3791342cc9520920dfe29f93de867ee075779
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2016-12-12 15:53:13 +0000

    Update install.sh

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,34 +1,43 @@
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $ROOT_MYSQL_PASSWORD"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $ROOT_MYSQL_PASSWORD"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password ${ROOT_MYSQL_PASSWORD}"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password ${ROOT_MYSQL_PASSWORD}"
 
-apt-get install expect mysql-server -y
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 5072E1F5
+echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee /etc/apt/sources.list.d/mysql57.list
+apt-get update
 
-if [ $IS_ON_DOCKER == true ]; then
-	service mysql start
-fi
+apt-get install mysql-server -y
+sudo update-alternatives --remove my.cnf /etc/mysql/my.cnf.migrated
+sudo service mysql start
 
-mysql_install_db
+apt-get install expect -y
+
 expect -c "
-
 set timeout 10
 spawn mysql_secure_installation
 
-expect \"Enter current password for root (enter for none):\"
+expect \"Enter password for user root:\"
 send \"$ROOT_MYSQL_PASSWORD\r\"
 
-expect \"Change the root password?\"
+expect \"Press y|Y for Yes, any other key for No:\"
+send \"y\r\"
+
+expect \"Please enter 0 = LOW, 1 = MEDIUM and 2 = STRONG:\"
+send \"0\r\"
+
+expect \"Change the password for root ? ((Press y|Y for Yes, any other key for No) :\"
 send \"n\r\"
 
-expect \"Remove anonymous users?\"
+expect \"Remove anonymous users? (Press y|Y for Yes, any other key for No) :\"
 send \"y\r\"
 
-expect \"Disallow root login remotely?\"
+expect \"Disallow root login remotely? (Press y|Y for Yes, any other key for No) :\"
 send \"y\r\"
 
-expect \"Remove test database and access to it?\"
+expect \"Remove test database and access to it? (Press y|Y for Yes, any other key for No) :\"
 send \"y\r\"
 
-expect \"Reload privilege tables now?\"
+expect \"Reload privilege tables now? (Press y|Y for Yes, any other key for No) :\"
 send \"y\r\"
 
 expect eof"
