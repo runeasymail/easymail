8f3aa7185de4db3a3de04db0e08df455d4365e64
GyunerZeki <me@gzeki.com>
2017-01-20 21:17:05 +0000

    finish the start up

--- autostart/install.sh
+++ autostart/install.sh
@@ -4,7 +4,7 @@ AUTOSTART_DIR="$CURRENT_DIR/autostart"
 
 cp $AUTOSTART_DIR/run.sh /run.sh
 
-echo "cd $EASY_MAIL_DIR/ManagementAPI && ./ManagementAPI" >> /run.sh
+echo "cd $EASY_MAIL_DIR/ManagementAPI && ./ManagementAPI &" >> /run.sh
 
 if [ $IS_ON_DOCKER == true ]; then 
   chmod +x /run.sh
