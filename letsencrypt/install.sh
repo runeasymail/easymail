a29070f9f7d888acf487dbf53eb2760d47f550a1
Yuksel Saliev <yuksel.saliev@gmail.com>
2017-02-25 12:45:15 +0200

    Use correct path

--- letsencrypt/install.sh
+++ letsencrypt/install.sh
@@ -9,7 +9,7 @@ openssl genrsa 4096 > account.key
 openssl genrsa 4096 > domain.key
 openssl req -new -sha256 -key domain.key -subj "/CN=${HOSTNAME}" > domain.csr
 mkdir -p /var/www/challenges/
-sed -i "s|# # __EASY_MAIL_INCLUDE_LETSENCRYPT__|include $CURRENT_DIR/letsencrpt/lets_encrypt.conf;|g" /etc/nginx/sites-enabled/roundcube
+sed -i "s|# __EASY_MAIL_INCLUDE_LETSENCRYPT__|include $CURRENT_DIR/letsencrypt/lets_encrypt.conf;|g" /etc/nginx/sites-enabled/roundcube
 python acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /var/www/challenges/ > ./signed.crt
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > intermediate.pem
 cat signed.crt intermediate.pem > chained.pem
