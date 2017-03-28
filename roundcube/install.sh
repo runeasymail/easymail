7969160058d4effc69838fe1c507a67f63e2c2e8
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-03-28 17:17:45 +0100

    Update install.sh

--- roundcube/install.sh
+++ roundcube/install.sh
@@ -2,7 +2,7 @@
 
 ROUNDCUBE_DIR="$CURRENT_DIR/roundcube"
 
-apt-get install -y language-pack-en-base software-properties-common
+apt-get install language-pack-en-base software-properties-common -y
 LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php -y
 apt-get install php7.0-fpm mcrypt php7.0-mcrypt php7.0-intl php7.0-mysql php7.0-mbstring php7.0-curl php7.0-zip php-xml php-xml-parser php7.0-cli php7.0-gd php-apcu php7.0-imap php-mail php-mail-mimedecode php-mime-type php-mail-mime -y
 
