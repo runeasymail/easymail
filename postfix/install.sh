d7ac8c3d869edaf0ed5d3b1ada3398f17d04f91c
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-25 14:03:25 +0100

    Update install.sh

--- postfix/install.sh
+++ postfix/install.sh
@@ -87,9 +87,11 @@ postfix_mysql_file "query = SELECT 1 FROM virtual_users WHERE email='%s'" mysql-
 postfix_mysql_file "query = SELECT destination FROM virtual_aliases WHERE source='%s'" mysql-virtual-alias-maps.cf
 postfix_mysql_file "query = SELECT to_address FROM recipient_bcc WHERE from_address='%s'" mysql-recipient-bcc-maps.cf
 
-if [ $IS_ON_DOCKER == true ]; then 
-	/etc/init.d/postfix start
-else 
-	service postfix start
-fi
+service postfix start
+
+#if [ $IS_ON_DOCKER == true ]; then 
+#	/etc/init.d/postfix start
+#else 
+#	service postfix start
+#fi
 
