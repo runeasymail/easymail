b4ff4496e8259a174bac3f74c12b52d23fa6932d
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-25 11:32:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -239,6 +239,9 @@ protocol lda {
 # Kill all processes (Apache) listening on port 80 because this may prevent the start of NGINX
 #fuser -k 80/tcp
 
+echo -e "Stop 1";
+exit;
+
 if [ $IS_ON_DOCKER == true ]; then
 	/usr/sbin/dovecot
 	/etc/init.d/postfix restart
