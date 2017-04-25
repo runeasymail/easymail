97a2a48d076f03fcc9a510f39387babbcae6c675
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-25 16:51:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -165,99 +165,13 @@ sed -i "s/admin@__EASYMAIL_HOSTNAME__/admin@$HOSTNAME/g" /etc/dovecot/conf.d/20-
 service nginx restart 
 service dovecot reload
 service postfix reload
-	# DKIM
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-# Install DKIM
-set -e
-
-apt-get install opendkim opendkim-tools -y
-
-echo "
-AutoRestart             Yes
-AutoRestartRate         10/1h
-UMask                   002
-Syslog                  yes
-SyslogSuccess           Yes
-LogWhy                  Yes
-Canonicalization        relaxed/simple
-ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-InternalHosts           refile:/etc/opendkim/TrustedHosts
-KeyTable                refile:/etc/opendkim/KeyTable
-SigningTable            refile:/etc/opendkim/SigningTable
-Mode                    sv
-PidFile                 /var/run/opendkim/opendkim.pid
-SignatureAlgorithm      rsa-sha256
-UserID                  opendkim:opendkim
-Socket                  inet:12301@localhost
-" > /etc/opendkim.conf
-
-echo 'SOCKET="inet:12301@localhost"' > /etc/default/opendkim
-
-echo "
-milter_protocol = 2
-milter_default_action = accept
-smtpd_milters = unix:/spamass/spamass.sock, inet:localhost:12301
-non_smtpd_milters = unix:/spamass/spamass.sock, inet:localhost:12301
-" >>  /etc/postfix/main.cf
-
-mkdir /etc/opendkim/keys -p
-
-echo "
-127.0.0.1
-localhost
-192.168.0.1/24
-*$HOSTNAME
-# add more domains
-#*<DOMAIN>
-" > /etc/opendkim/TrustedHosts
-
-echo "
-mail._domainkey.$HOSTNAME $HOSTNAME:mail:/etc/opendkim/keys/$HOSTNAME/mail.private
-# add more domains in following format.
-#mail._domainkey.<DOMAIN> <DOMAIN>:mail:/etc/opendkim/keys/<DOMAIN>/mail.private
-" > /etc/opendkim/KeyTable
-
-echo "
-*@$HOSTNAME mail._domainkey.$HOSTNAME
-# Add more domains in following format.
-#*@<DOMAIN> mail._domainkey.<DOMAIN>
-" > /etc/opendkim/SigningTable
-
-cd /etc/opendkim/keys && mkdir $HOSTNAME && cd $HOSTNAME
-opendkim-genkey -s mail -d $HOSTNAME
-chown opendkim:opendkim mail.private
-
-echo "Step 1";
-exit;
-
-service postfix restart
-service opendkim restart
-
-echo "Create mail._domainkey.$HOSTNAME TXT record with following content"
-echo ""
-tail mail.txt  -n 1 | awk '{ print $1 }' | sed 's/p=/v=DKIM1; k=rsa; p=/g'
-echo ""	
-	
-	
-	
-	
-	
-	
+	# DKIM		
 bash $CURRENT_DIR/dkim/install.sh
-
-
+	# Management API
 sed -i "s/__EASYMAIL_HOSTNAME__/$HOSTNAME/g" /opt/easymail/ManagementAPI/config.ini
-# restart the ManagementAPI 
 pkill ManagementAPI && cd /opt/easymail/ManagementAPI && ./ManagementAPI &
+
+
 
 echo -e "\n----------------------"
 echo "Database - access:"
