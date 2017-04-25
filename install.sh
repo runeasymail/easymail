df79c2d2eb7fb2e15403fe671f0477cf505e3de8
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-25 14:10:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -124,6 +124,8 @@ bash $CURRENT_DIR/roundcube/install.sh
 bash $CURRENT_DIR/autoconfig/install.sh
 bash $CURRENT_DIR/spamassassin/install.sh
 bash $CURRENT_DIR/autostart/install.sh
+echo "Stop 1";
+exit;
 bash $CURRENT_DIR/ManagementAPI/install.sh
 
 # after that part all the code should be executed for each container too.
