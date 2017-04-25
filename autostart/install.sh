8c064704121f85250dd1d48a19740c50dbd818bd
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-25 16:13:40 +0100

    Update install.sh

--- autostart/install.sh
+++ autostart/install.sh
@@ -4,10 +4,6 @@ set -e
 AUTOSTART_DIR="$CURRENT_DIR/autostart"
 
 cp $AUTOSTART_DIR/run.sh /run.sh
-
-if [ $IS_ON_DOCKER == true ]; then 
-  chmod +x /run.sh
-  sh /run.sh
-fi
+chmod +x /run.sh
 
 echo "cd $EASY_MAIL_DIR/ManagementAPI && ./ManagementAPI &" >> /run.sh
