7f7821967a750653493ea8797a3f5bd18530cb02
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-28 16:33:19 +0100

    Update install.sh

--- ManagementAPI/install.sh
+++ ManagementAPI/install.sh
@@ -26,8 +26,15 @@ password:$MANAGEMENT_API_PASSWORD
 
 
 cp $CURRENT_DIR/ManagementAPI/ManagementAPI-nginx.conf $MANAGEMENT_API_DIR/ManagementAPI-nginx.conf
+cp $CURRENT_DIR/ManagementAPI/renew_letsencrypt_certificate.sh $MANAGEMENT_API_DIR/renew_letsencrypt_certificate.sh
 
 sed -i "s|# __EASY_MAIL_INCLUDE__|include $MANAGEMENT_API_DIR/ManagementAPI-nginx.conf;|g" /etc/nginx/sites-enabled/roundcube
+
+crontab <<EOF
+0 0 1 * * sh $MANAGEMENT_API_DIR/renew_letsencrypt_certificate.sh
+EOF
+
+service cron start
 
 ./ManagementAPI &
 service nginx reload
