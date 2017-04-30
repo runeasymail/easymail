fd237d812ecdfc63daa421d8b71fee66ae62ebaf
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-04-30 01:48:48 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -49,6 +49,6 @@ sed -i "s/__EASYMAIL_HOSTNAME__/$HOSTNAME/g" /opt/easymail/ManagementAPI/config.
 pkill ManagementAPI && cd /opt/easymail/ManagementAPI && ./ManagementAPI &
 
 # Add new configurations to easymail config file
-sed -i "s/roundcube_url:.*/roundcube_url:https:\/\/$HOSTNAME\//" $EASYMAIL_CONFIG
-sed -i "s/roundcube_username:.*/roundcube_username:$ADMIN_EMAIL/" $EASYMAIL_CONFIG
+sed -i "s/roundcube_web_url:.*/roundcube_web_url:https:\/\/$HOSTNAME\//" $EASYMAIL_CONFIG
+sed -i "s/roundcube_web_username:.*/roundcube_web_username:$ADMIN_EMAIL/" $EASYMAIL_CONFIG
 sed -i "s/api_url:.*/api_url:https:\/\/$HOSTNAME\/api/" $EASYMAIL_CONFIG
