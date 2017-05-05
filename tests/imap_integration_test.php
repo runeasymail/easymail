6fc70871327c62afb483d954abcebdf0980f5476
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-05-05 15:04:04 +0100

    Create imap_integration_test.php

---
+++ tests/imap_integration_test.php
@@ -0,0 +1,39 @@
+<?php
+
+require 'PHPMailer/PHPMailerAutoload.php';
+require 'config.php';
+
+// Define variables
+$subject = "Test";
+$from = "EasyMail";
+$body = "Test.";
+
+/*=============================== IMAP ===============================*/
+        // STARTTLS on port 143
+$imap_stream = imap_open("{".$hostname.":143/imap/tls/novalidate-cert}INBOX", $email, $password) or die("Can't connect over IMAP, STARTTLS on port 143: ".imap_last_error());
+$inbox = (array) imap_check($imap_stream);
+$messages_in_inbox = $inbox['Nmsgs'];
+imap_mail($email, $subject, $body) or die("Can't send email over IMAP, STARTTLS on port 143: ".imap_last_error());
+
+sleep(1);
+$inbox = (array) imap_check($imap_stream);
+
+if ($messages_in_inbox == $inbox['Nmsgs']) {
+        die("Message not received over IMAP, STARTTLS on port 143: ".imap_last_error());
+}
+
+$messages_in_inbox = $inbox['Nmsgs'];
+imap_close($imap_stream);
+
+        // SSL on port 993
+$imap_stream = imap_open("{".$hostname.":993/imap/ssl/novalidate-cert}INBOX", $email, $password) or die("Can't connect over IMAP, SSL on port 993: ".imap_last_error());
+imap_mail($email, $subject, $body) or die("Can't send email over IMAP, SSL on port 993: ".imap_last_error());
+
+sleep(1);
+$inbox = (array) imap_check($imap_stream);
+
+if ($messages_in_inbox == $inbox['Nmsgs']) {
+        die("Message not received over IMAP, SSL on port 993: ".imap_last_error());
+}
+
+imap_close($imap_stream);
