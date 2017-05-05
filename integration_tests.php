b1dbd84ef7e08495fd6210da8e58122e895a276d
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-05-05 14:41:58 +0100

    Update integration_tests.php

--- integration_tests.php
+++ integration_tests.php
@@ -8,14 +8,14 @@ $email = "admin@".$domain;
 $password = "__ROUNDCUBE_WEB_PASSWORD__";
 $subject = "Test";
 $from = "EasyMail";
-$message = "Test.";
+$body = "Test.";
 
 /*=============================== IMAP ===============================*/
         // STARTTLS on port 143
 $imap_stream = imap_open("{".$domain.":143/imap/tls/novalidate-cert}INBOX", $email, $password) or die("Can't connect over IMAP, STARTTLS on port 143: ".imap_last_error());
 $inbox = (array) imap_check($imap_stream);
 $messages_in_inbox = $inbox['Nmsgs'];
-imap_mail($email, $subject, $message) or die("Can't send email over IMAP, STARTTLS on port 143: ".imap_last_error());
+imap_mail($email, $subject, $body) or die("Can't send email over IMAP, STARTTLS on port 143: ".imap_last_error());
 sleep(1);
 $inbox = (array) imap_check($imap_stream);
 
@@ -28,7 +28,7 @@ imap_close($imap_stream);
 
         // SSL on port 993
 $imap_stream = imap_open("{".$domain.":993/imap/ssl/novalidate-cert}INBOX", $email, $password) or die("Can't connect over IMAP, SSL on port 993: ".imap_last_error());
-imap_mail($email, $subject, $message) or die("Can't send email over IMAP, SSL on port 993: ".imap_last_error());
+imap_mail($email, $subject, $body) or die("Can't send email over IMAP, SSL on port 993: ".imap_last_error());
 sleep(1);
 $inbox = (array) imap_check($imap_stream);
 
@@ -57,7 +57,7 @@ $mail->Host = $domain;
 $mail->From = $email;
 $mail->FromName = $from;
 $mail->Subject = $subject;
-$mail->Body = $message;
+$mail->Body = $body;
 $mail->addAddress($email, $from);
 
 if (!$mail->send()) {
@@ -91,7 +91,7 @@ $mail->Host = $domain;
 $mail->From = $email;
 $mail->FromName = $from;
 $mail->Subject = $subject;
-$mail->Body = $message;
+$mail->Body = $body;
 $mail->addAddress($email, $from);
 
 if (!$mail->send()) {
@@ -129,7 +129,7 @@ $mail->Password = $password;
 $mail->setFrom($email, $from);
 $mail->addAddress($email, $from);
 $mail->Subject = $subject;
-$mail->msgHTML($message);
+$mail->msgHTML($body);
 
 if (!$mail->send()) {
     die("Can't send email over SMTP, STARTTLS on port 587: ".$mail->ErrorInfo);
@@ -165,7 +165,7 @@ $mail->Password = $password;
 $mail->setFrom($email, $from);
 $mail->addAddress($email, $from);
 $mail->Subject = $subject;
-$mail->msgHTML($message);
+$mail->msgHTML($body);
 
 if (!$mail->send()) {
     die("Can't send email over SMTP, SSL on port 465: ".$mail->ErrorInfo);
