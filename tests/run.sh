5ae0c5d375bc46125c6037d283202652132c15ca
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-05-05 18:14:37 +0100

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -2,6 +2,10 @@
 
 export $PASSWORD='PUT password here'
 
+apt-get install git -y
+
 cd /easymail/tests/
+
 git clone https://github.com/PHPMailer/PHPMailer.git
+
 sed -i "s#__ROUNDCUBE_WEB_PASSWORD__#$PASSWORD#g" $EASY_MAIL_DIR/tests/config.php
