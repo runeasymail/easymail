024114ab638c9803f43fcc5924c08d0adb9fe0fe
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-05-06 14:06:35 +0100

    Update install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -1,12 +1,12 @@
 export EASY_MAIL_DIR="/opt/easymail"
-export PHP_MAILER_TAG="v5.2.23"
+export PHP_MAILER_TAG="5.2.23"
 export PASSWORD=$(cat "$EASY_MAIL_DIR/config.ini" | grep roundcube_web_password: | awk -F':' '{ print $2;}')
 
 apt-get install wget -y
 
 cd /easymail/tests/
 
-wget -O PHPMailer.tar.gz https://github.com/PHPMailer/PHPMailer/archive/$PHP_MAILER_TAG.tar.gz
+wget -O PHPMailer.tar.gz https://github.com/PHPMailer/PHPMailer/archive/v$PHP_MAILER_TAG.tar.gz
 tar -xvzf PHPMailer.tar.gz
 mv PHPMailer-$PHP_MAILER_TAG PHPMailer
 
