69f3fbcc654440b395e951fd03b0b54dba93949f
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-05-06 13:51:36 +0100

    Update install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -1,10 +1,12 @@
 export EASY_MAIL_DIR="/opt/easymail"
 export PASSWORD=$(cat "$EASY_MAIL_DIR/config.ini" | grep roundcube_web_password: | awk -F':' '{ print $2;}')
 
-apt-get install git -y
+apt-get install wget -y
 
 cd /easymail/tests/
 
-git clone https://github.com/PHPMailer/PHPMailer.git
+wget -O PHPMailer.tar.gz https://github.com/PHPMailer/PHPMailer/archive/v5.2.23.tar.gz
+tar -xvzf PHPMailer.tar.gz
+chmod +x PHPMailer
 
 sed -i "s#__ROUNDCUBE_WEB_PASSWORD__#$PASSWORD#g" /easymail/tests/config.php
