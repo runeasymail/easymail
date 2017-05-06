1aa112867db318dcadb752fcdb57d4acf400f738
Pavel Tashev <pavel-tashev@users.noreply.github.com>
2017-05-06 21:31:44 +0100

    Update config.php

--- tests/config.php
+++ tests/config.php
@@ -1,5 +1,12 @@
 <?php
 
+// Define variables
 $hostname = "test.example.com";
 $email = "admin@".$hostname;
 $password = "__ROUNDCUBE_WEB_PASSWORD__";
+
+// Define functions
+function die2($input) {
+  echo $input; 
+  exit(1);
+}
