2a118338f8f39614a9f06f4dc38905d284ff6536
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-07-05 18:42:16 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,12 @@
 set -e
 
+# Check for min system requirements
+if (($(($(free -mt|awk '/^Total:/{print $2}')*1)) <= 900)); then
+   echo -e "EasyMail has been stopped because of the following minimum requirements:\n";
+   echo -e "- RAM (or RAM + SWAP) >= 1GB\n\n";
+   exit;
+fi
+
 bash /run.sh; 
 
 # run only once
