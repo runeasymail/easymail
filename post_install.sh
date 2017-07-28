f456ea81438f5ff465dede4cabe0dca8531c499a
GyunerZeki <me@gzeki.com>
2017-07-28 19:35:28 +0100

    Simplify configs management

--- post_install.sh
+++ post_install.sh
@@ -99,6 +99,9 @@ UPDATE \`virtual_users\`
 SET \`email\`='$ADMIN_EMAIL', \`password\`='$ADMIN_PASSWORD'
 WHERE \`id\`='1';
 
+# this should be fixed
+#ALTER USER 'roundcube_user'@'localhost' IDENTIFIED BY '$ROUNDCUBE_MYSQL_PASSWORD';
+
 ALTER USER 'mailuser'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_MYSQL_PASSWORD';
 EOF
