122d2261355eda8e19efe29bdaa2d87468d3858e
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-09-22 23:18:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,7 +92,7 @@ api_username:__EASYMAIL_MANAGEMENT_API_USERNAME__
 api_password:__EASYMAIL_MANAGEMENT_API_PASSWORD__
 "  >> $EASY_MAIL_DIR/config.ini
 
-# Set HOSTNAME for Dovecot
+# Keep the hostname dir in "/easymail_mail_dir" because mail dir is volume it will remove it  
 mv /var/mail/vhosts/__EASYMAIL_HOSTNAME__ /easymail_mail_dir
  
 cp $CURRENT_DIR/post_install.sh $EASY_MAIL_DIR/post_install.sh
