a621db5b4f070757c48b1ae03af466278317dcd2
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-09-30 13:06:59 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -7,10 +7,16 @@ if (($(($(free -mt|awk '/^Total:/{print $2}')*1)) <= 700)); then
    exit;
 fi
 
-bash /run.sh; 
-
 # run only once
 ALREADY_RUN_POST_INSTALL_FILE="/opt/easymail/already-run-post-install.txt"
+
+# It has to be done only on first run.
+if [ ! -e "$ALREADY_RUN_POST_INSTALL_FILE" ]; then
+  # move the inital mysql data files
+  mv /easymail_mysql_lib_dir /var/lib/mysql
+fi
+
+bash /run.sh; 
 
 if [ -e "$ALREADY_RUN_POST_INSTALL_FILE" ]; then
   exit;
