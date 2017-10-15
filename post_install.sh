5a3f9ab197c5f991e3252c30388939d4bb547cea
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-10-15 14:13:11 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -18,6 +18,9 @@ fi
 
 touch $ALREADY_RUN_POST_INSTALL_FILE
 
+# create payload directory
+mkdir -p /opt/easymail/data/{mysql,dovecot}
+
 # Get variables
 export EASYMAIL_CONFIG="/opt/easymail/config.ini"
 
