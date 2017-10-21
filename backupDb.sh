ce98f209018ec31bfa542ceec591292d0add8707
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-10-21 17:17:20 +0100

    Create backupDb.sh

---
+++ backupDb.sh
@@ -0,0 +1,10 @@
+export EASYMAIL_CONFIG="/opt/easymail/config.ini"
+
+export HOSTNAME=$(cat "$EASYMAIL_CONFIG" | grep mysql_easymail_hostname: | awk -F':' '{ print $2;}')
+export DATABASE=$(cat "$EASYMAIL_CONFIG" | grep mysql_easymail_database: | awk -F':' '{ print $2;}')
+export DATABASE2=$(cat "$EASYMAIL_CONFIG" | grep mysql_roundcube_database: | awk -F':' '{ print $2;}')
+
+export USERNAME=$(cat "$EASYMAIL_CONFIG" | grep mysql_root_username: | awk -F':' '{ print $2;}')
+export PASSWORD=$(cat "$EASYMAIL_CONFIG" | grep mysql_root_password: | awk -F':' '{ print $2;}')
+
+mysqldump -u$USERNAME -p$PASSWORD -h$HOSTNAME --databases $DATABASE $DATABASE2 | sed 's/ AUTO_INCREMENT=[0-9]*//g'  > /opt/easymail/dbBackup.sql
