51a932006025170779b56a5ad43fa41a97630077
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-11-09 00:04:16 +0000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -164,6 +164,7 @@ sleep 10
 export SSL_CA_BUNDLE_FILE="/opt/easymail/data/ssl/public.pem"
 export SSL_PRIVATE_KEY_FILE="/opt/easymail/data/ssl/private.pem"
 
+chmod 0777 -R /opt/easymail/data/ssl
 openssl req -new -x509 -days 365000 -nodes -subj "/C=/ST=/L=/O=/CN=EasyMail" -out "$SSL_CA_BUNDLE_FILE" -keyout "$SSL_PRIVATE_KEY_FILE"
 
 # new SSL location for Postfix
