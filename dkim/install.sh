547d9f07b418af6e2c58320bd667fbeebd803ac3
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2017-12-14 18:10:54 +0000

    Update install.sh

--- dkim/install.sh
+++ dkim/install.sh
@@ -56,12 +56,12 @@ dpkg -i libopendbx1-mysql_1.4.6-9build1_amd64.deb
 
 mysql -h $MYSQL_HOSTNAME -u$ROOT_MYSQL_USERNAME -p$ROOT_MYSQL_PASSWORD << EOF
 USE $MYSQL_DATABASE;
-CREATE TABLE IF NOT EXISTS `dkim` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `domain_name` varchar(50) NOT NULL,
-  `selector` varchar(50) NOT NULL,
-  `private_key` text NOT NULL,
-  `public_key` text NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS dkim (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  domain_name varchar(50) NOT NULL,
+  selector varchar(50) NOT NULL,
+  private_key text NOT NULL,
+  public_key text NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 EOF
