11f9da12e4b16f782031c6af2d072766a2a3c069
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2019-08-04 15:42:24 +0300

    Update exportDb.sh

--- exportDb.sh
+++ exportDb.sh
@@ -1,10 +1,9 @@
 export EASYMAIL_CONFIG="/opt/easymail/config.ini"
 
-export HOSTNAME=$(cat "$EASYMAIL_CONFIG" | grep mysql_easymail_hostname: | awk -F':' '{ print $2;}')
 export DATABASE=$(cat "$EASYMAIL_CONFIG" | grep mysql_easymail_database: | awk -F':' '{ print $2;}')
 export DATABASE2=$(cat "$EASYMAIL_CONFIG" | grep mysql_roundcube_database: | awk -F':' '{ print $2;}')
 
 export USERNAME=$(cat "$EASYMAIL_CONFIG" | grep mysql_root_username: | awk -F':' '{ print $2;}')
 export PASSWORD=$(cat "$EASYMAIL_CONFIG" | grep mysql_root_password: | awk -F':' '{ print $2;}')
 
-mysqldump -u$USERNAME -p$PASSWORD -h$HOSTNAME --databases $DATABASE $DATABASE2 > /opt/easymail/data/mysql/db.sql
+mysqldump -u$USERNAME -p$PASSWORD --databases $DATABASE $DATABASE2 > /opt/easymail/data/mysql/db.sql
