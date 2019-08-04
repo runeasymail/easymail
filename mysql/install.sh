6cebf5f8cc48fb24bd738fdb1beac21cf3461ef6
Gyuner Zeki <GyunerZeki@users.noreply.github.com>
2019-08-04 10:16:25 +0300

    Update install.sh

--- mysql/install.sh
+++ mysql/install.sh
@@ -4,8 +4,8 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 
 debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password ${ROOT_MYSQL_PASSWORD}"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password ${ROOT_MYSQL_PASSWORD}"
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password ${ROOT_MYSQL_PASSWORD}"
+#debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password ${ROOT_MYSQL_PASSWORD}"
 
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5
 echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | tee -a /etc/apt/sources.list.d/mysql.list
@@ -13,12 +13,13 @@ apt-get update -y && apt-get install mysql-server expect -y
 update-alternatives --remove my.cnf /etc/mysql/my.cnf.migrated
 service mysql start
 
+#expect \"Enter password for user root:\"
+#send \"$ROOT_MYSQL_PASSWORD\r\"
+
+
 expect -c "
 set timeout 10
 spawn mysql_secure_installation
-
-expect \"Enter password for user root:\"
-send \"$ROOT_MYSQL_PASSWORD\r\"
 
 expect \"Press y|Y for Yes, any other key for No:\"
 send \"n\r\"
